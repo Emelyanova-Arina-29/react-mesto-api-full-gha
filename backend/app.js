bac4c019059be095bf1d12917c34647ffe7fc48f
Arina Emelyanova <arina.em29@yandex.ru>
2023-06-08 18:30:29 +0500

    7

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const router = require('./routes/routes');
 const errorsHandler = require('./errors/errorsHandler');
@@ -16,12 +16,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   max: 100,
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
-// app.use(limiter);
+app.use(limiter);
 
 app.use(requestLogger);
 
