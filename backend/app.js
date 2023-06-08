de89269d38f5934bbebbbd86e09b813ce3590f8f
Arina Emelyanova <arina.em29@yandex.ru>
2023-06-08 18:59:47 +0500

    8

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const router = require('./routes/routes');
 const errorsHandler = require('./errors/errorsHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const corsSettings = {
+  origin: ['https://mesto.arina.nomoredomains.rocks',
+    'http://localhost:3000',
+    'http://mesto.arina.nomoredomains.rocks',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization', 'Origin'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(cors(corsSettings));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
