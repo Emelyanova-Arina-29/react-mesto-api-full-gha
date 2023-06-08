182ee3f075cec4b4b0adb11f326296b5aee27eed
Arina Emelyanova <arina.em29@yandex.ru>
2023-06-08 17:57:36 +0500

    3

--- backend/models/user.js
+++ backend/models/user.js
@@ -7,11 +7,10 @@ const {
 
 const userSchema = new mongoose.Schema({
   name: {
-    required: true,
     type: String,
     minlength: 2,
     maxlength: 30,
-    // default: 'Жак-Ив Кусто',
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
